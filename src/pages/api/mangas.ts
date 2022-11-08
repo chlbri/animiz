07b3233682f246be1dcf2baa3e2e7a00fb445c11
chlbri <bri_lvi@icloud.com>
2022-11-08 22:52:47 +0000

    ok

--- src/pages/api/mangas.ts
+++ src/pages/api/mangas.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro';
 import { filterService } from 'src/services/filter.machine';
 
+//TODO : arrange
 export const get: APIRoute = ({ url }) => {
   const params = url.searchParams;
   console.log('params =>', Array.from(params.values()));
